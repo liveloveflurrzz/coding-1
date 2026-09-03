44f51686333e250e0f7232a940fe59e0cc0b3479
liveloveflurrzz <jsmith-Terry815@boyslatin.net>
2026-09-03 15:05:18 +0000

    Class Activity

---
+++ unit 1/variableActivity.py
@@ -0,0 +1,40 @@
+# VARIABLES - a container that stores data. 
+# you can name your container almost anyhting
+# you want, so long as it is descriptive
+
+#Variable Naming Rules
+# 1. A variable MUST start with letter and cannot start with a number
+# 2. A varibale can be a mixed of letters and numbers BUT not symbols
+# 3. A variable CANNOT be a python keyword
+# 4. Variables with multiple words do not have spaces
+
+
+appleCost = 1.00
+numberOfApple = 10
+orange = 1.05
+user = "Jayden
+hasfriedonions = true
+
+
+Ja3SneakersCount = 10
+Ja3SneakersPrice = 150.00
+Ja3SneakersSKU = "91022382913"
+
+
+# When we want to create a variable, we make up a word,
+# use the equal sign and then assign it a data type.
+# whenever the computer sees our variable word it will
+# always use the data inside.
+
+
+applesAtStore = 3
+studentsInClass = 15
+AdditionalTravelInsurance = "True"
+Bronzemembership = 15.99
+Silvermembership = 25.99
+Goldmembership = 35.99
+Water = 1.50
+OJ = 2.50
+Soda = 3.50
+
+
