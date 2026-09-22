7f48b88990a7e58c24181e01befa061c56300abe
liveloveflurrzz <jsmith-Terry815@boyslatin.net>
2026-09-22 15:08:34 +0000

    classwork/notes

---
+++ Unit 2/builtinFunctions.py
@@ -0,0 +1,25 @@
+# Data casting functions
+# these are built in functions (pre-written instructions)
+# that change data types from one form into another
+
+# str() - this datacasting function allows you to change 
+# any data type into a string
+
+# Function - simply put ; a code block of
+# Instructions for computers to follow.
+
+# Built- in Function - a code block of
+# instruction for a computer to follow
+# that was already written for us.
+# PRE-WRITTEN CODE INSTRUCTIONS
+
+# string concatenation - combining string data types 
+#with one another. We use the plus sign (+) operator
+# to combine strings together
+
+# int()- Any data type passed into the brackets will 
+# # be converted into a integer (whole number)
+
+num1 = input ("type in a number: ")
+# input always returns a string
+print (4 + int(num1) 
--- unit 1/operatorsActivity.py
+++ unit 1/operatorsActivity.py
@@ -10,3 +10,44 @@ print("Coding1"== "coding1")
 #false
 
 print("0" )
+
+
+
+# A cash register program that needs to calculate 
+# the final balance of two items.
+book = 10.99
+tablet= 399.99
+
+print(book + tablet) 
+
+# A program checking if you have spent over 100 dollars
+# before giving you a discount
+
+print (cart > amountToGetDiscount)
+
+# A program that applies 7% sales tax
+# to your $200 dollar shoes. how much
+#are you paying?
+Shoes = 200.00
+Tax = 0.07
+
+print (200.00 * 0.07)
+
+
+# A college app program that checks if a
+# GPA is above 85 AND that you have a 
+# letter of recommendation to admit you in
+
+GPA = 86
+
+Recommendation = True
+
+print ( GPA > 85 and recommendation == True)
+
+# A school safety program that checks if at least
+# 1 of 2 emergency contact number works.
+
+parentContact1 = ""
+parentContact2 = "215 777 8787"
+
+print(parentContact1 == True or parentContact2 == True)
